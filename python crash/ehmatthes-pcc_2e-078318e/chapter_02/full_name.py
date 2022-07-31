2f164ed4dab89377e71ee34625af2e5f344a82a5
Akaflyaway <akaflyawa2@gmail.com>
2022-07-31 22:33:25 +0900

    All file uploaded

--- python crash/ehmatthes-pcc_2e-078318e/chapter_02/full_name.py
+++ python crash/ehmatthes-pcc_2e-078318e/chapter_02/full_name.py
@@ -1,5 +1,11 @@
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
+#print(f"Hello, {full_name.title()}!")
+
 message = f"Hello, {full_name.title()}!"
 print(message)
+
+
+#Tell I did my homework !
+
