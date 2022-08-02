69d12c9304c1805d3ac840c32030bc30ad8d10ac
Akaflyaway <akaflyawa2@gmail.com>
2022-08-02 23:31:04 +0900

    added all files

--- python-crash/crashbook/chapter_03/bicycles.py
+++ python-crash/crashbook/chapter_03/bicycles.py
@@ -1,4 +1,14 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-message = f"My first bicycle was a {bicycles[0].title()}."
+bicycles = ['trek','cannondale','redline','specialized']
+print(bicycles)
 
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+print(bicycles[1])
+
+print(bicycles[3])
+print(bicycles[-1])
+
+message = f"My first bicycle was a {bicycles[0].title()}."
 print(message)
--- python-crash/crashbook/chapter_03/motorcycles.py
+++ python-crash/crashbook/chapter_03/motorcycles.py
@@ -1,7 +1,14 @@
-motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
+# motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
+# print(motorcycles)
+
+# too_expensive = 'ducati'
+# motorcycles.remove(too_expensive)
+# print(motorcycles)
+# print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+
+motorcycles = ['honda','yamaha','suzuki']
 print(motorcycles)
 
-too_expensive = 'ducati'
-motorcycles.remove(too_expensive)
+motorcycles[0] = 'ducati'
 print(motorcycles)
-print(f"\nA {too_expensive.title()} is too expensive for me.")
